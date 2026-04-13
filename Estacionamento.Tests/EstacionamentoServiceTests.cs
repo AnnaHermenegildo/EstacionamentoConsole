373f9f806d700a06ff18a0e469b2874497b40acd
AnnaHermenegildo <hermenegildoanna@gmail.com>
2026-04-13 13:13:19 -0300

    Novos de testes do estacionamento

--- Estacionamento.Tests/EstacionamentoServiceTests.cs
+++ Estacionamento.Tests/EstacionamentoServiceTests.cs
@@ -3,15 +3,14 @@ using EstacionamentoConsole.Models;
 
 namespace Estacionamento.Tests
 {
+    [TestFixture]
     public class EstacionamentoServiceTests
     {
         [Test]
         public void Deve_Registrar_Entrada_De_Veiculo()
         {
             var estacionamento = new EstacionamentoConsole.Models.Estacionamento(5, 5);
-
             var veiculo = new Carro("ABC1234", "Toyota", "Corolla", "Prata");
-
             var vaga = estacionamento.ObterVagaDisponivel(veiculo);
 
             Assert.That(vaga, Is.Not.Null);
@@ -26,10 +25,11 @@ namespace Estacionamento.Tests
         {
             var estacionamento = new EstacionamentoConsole.Models.Estacionamento(5, 5);
             var veiculo = new Carro("ABC1234", "Toyota", "Corolla", "Prata");
-
             var vaga = estacionamento.ObterVagaDisponivel(veiculo);
 
+            Assert.That(vaga, Is.Not.Null);
             estacionamento.RegistrarEntrada(veiculo, vaga!);
+
             estacionamento.RegistrarSaida(veiculo, vaga!);
 
             Assert.That(vaga!.Ocupada, Is.False);
@@ -54,6 +54,38 @@ namespace Estacionamento.Tests
             var vaga = estacionamento.ObterVagaDisponivel(veiculo);
 
             Assert.That(vaga, Is.Not.Null);
+        }
+
+        [Test]
+        public void Deve_Diminuir_Quantidade_De_Vagas_De_Carro_Apos_Entrada()
+        {
+            var estacionamento = new EstacionamentoConsole.Models.Estacionamento(2, 0);
+            var veiculo = new Carro("ABC1234", "Toyota", "Corolla", "Prata");
+            var vaga = estacionamento.ObterVagaDisponivel(veiculo);
+
+            Assert.That(vaga, Is.Not.Null);
+
+            estacionamento.RegistrarEntrada(veiculo, vaga!);
+
+            Assert.That(estacionamento.VagasDisponiveisParaCarro(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Deve_Retornar_Null_Quando_Nao_Houver_Vaga_Para_Carro()
+        {
+            var estacionamento = new EstacionamentoConsole.Models.Estacionamento(1, 0);
+
+            var carro1 = new Carro("ABC1234", "Toyota", "Corolla", "Prata");
+            var carro2 = new Carro("DEF5678", "Honda", "Civic", "Preto");
+
+            var vaga1 = estacionamento.ObterVagaDisponivel(carro1);
+
+            Assert.That(vaga1, Is.Not.Null);
+            estacionamento.RegistrarEntrada(carro1, vaga1!);
+
+            var vaga2 = estacionamento.ObterVagaDisponivel(carro2);
+
+            Assert.That(vaga2, Is.Null);
         }
     }
 }
--- Estacionamento.Tests/obj/Debug/net9.0/Estacionamento.Tests.AssemblyInfo.cs
+++ Estacionamento.Tests/obj/Debug/net9.0/Estacionamento.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estacionamento.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0097a8f06410fe7f3f67cfdb5cf6ee5a220e2f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estacionamento.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estacionamento.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
